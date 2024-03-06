32f4d7f992c164452b5c635826ce42eab6059877
edinelsonslima <edinelsonlima86@gmail.com>
2024-03-05 23:30:37 -0300

    feat!: create new environments variables BREAKING CHANGING: changes parse variables for safeParse, closing node process in erros

--- src/app/settings.ts
+++ src/app/settings.ts
@@ -1,9 +1,30 @@
+/* eslint-disable no-console */
+import { env as environments, exit } from 'node:process';
+
 import { z } from 'zod';
 
 const DEFAULT_PORT = 3000;
 
 const schema = z.object({
   PORT: z.string().transform((port) => Number(port) || DEFAULT_PORT),
+
+  DB_USER: z.string().min(1, 'is required'),
+  DB_PASS: z.string().min(1, 'is required'),
+  DB_SERVER: z.string().min(1, 'is required'),
+  DB_DATABASE: z.string().min(1, 'is required'),
+  DB_PORT: z.string().transform((port) => Number(port) || 1433),
 });
 
-export const env = { ...schema.parse(process.env) };
+const result = schema.safeParse(environments);
+
+if (!result.success) {
+  const errors = result.error.errors
+    .map((err) => `=> ${err.path} ${err.message}`)
+    .join(' \n');
+
+  console.error(`environment variables error: \n%s`, errors);
+  exit(1);
+}
+
+const env = result.data;
+export { env };
