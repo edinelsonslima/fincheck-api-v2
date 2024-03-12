077d6b3c1350d920f36289d7794e2bc32e578934
edinelsonslima <edinelsonlima86@gmail.com>
2024-03-12 20:43:35 -0300

    feat: add user id in global types express request

--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -1,11 +1,10 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable @typescript-eslint/naming-convention */
 import { ConnectionPool } from 'mssql';
 
 declare global {
   namespace Express {
     interface Request {
       db: ConnectionPool;
+      userId: string;
     }
   }
 }
