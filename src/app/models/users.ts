87ce70b3dce19d55e3c7b08679b7307ab674c6a6
edinelsonslima <edinelsonlima86@gmail.com>
2024-03-12 20:55:45 -0300

    refactor!: move query for create table - create method for get user by email - create method for insert users in table

--- src/app/models/users.ts
+++ src/app/models/users.ts
@@ -1,80 +1,41 @@
 import { db, type IDatabase } from '@services/database';
-import { IUser, IUserFactory, UserFactory } from 'app/factories/user';
+import { IUser, IUserFactory, userFactory } from 'app/factories/user';
 
-interface IGetUser {
-  id?: string;
-  email?: string;
-  name?: string;
+interface IUserCreate {
+  name: string;
+  email: string;
+  password: string;
 }
 
 class UserModel {
   constructor(
     private readonly db: IDatabase,
-    private readonly userFactory: IUserFactory
+    private readonly factory: IUserFactory
   ) {
-    this.db = db;
-
-    this.db.query`
-      CREATE TABLE IF NOT EXISTS users (
-        id VARCHAR(255) PRIMARY KEY NOT NULL UNIQUE DEFAULT NEWID(),
-        name VARCHAR(255),
-        email VARCHAR(255) unique,
-        password VARCHAR(255),
-        created_at DATETIME DEFAULT GETDATE(),
-        updated_at DATETIME DEFAULT GETDATE(),
-      )
-    `;
+    this.db.createTable('users');
   }
 
-  public async get<T extends IUser>(filter?: IGetUser) {
-    try {
-      filter = JSON.parse(JSON.stringify(filter));
+  public async findOneByEmail(email: string) {
+    const result = await this.db.query<IUser>`
+      SELECT TOP 1 * FROM users
+      WHERE email = ${email};
+    `;
 
-      if (!filter) {
-        const users = await this.db.query<T>`
-          SELECT *
-          FROM users;
-        `;
-
-        return new this.userFactory(users).toJSON();
-      }
-
-      const user = await this.db.query<T>`
-        SELECT *
-        FROM users
-        WHERE id = ${filter.id};
-      `;
-
-      return new this.userFactory(user).toJSON();
-    } catch {
-      return null;
-    }
+    return this.factory.toObject(result);
   }
 
-  public create(name: string, email: string, password: string) {
-    return this.db.query`
-      INSERT INTO users (name, email, password, created_at, updated_at)
-      VALUES (${name}, ${email}, ${password}, GETDATE(), GETDATE());
+  public async create({ email, name, password }: IUserCreate) {
+    const result = await this.db.query<IUser>`
+      INSERT INTO users (name, email, password)
+      OUTPUT INSERTED.*
+      VALUES (${name} , ${email} , ${password});
     `;
-  }
 
-  public update(id: string, name: string, email: string, password: string) {
-    return this.db.query`
-      UPDATE users
-      SET name = ${name}, email = ${email}, password = ${password}, updated_at = GETDATE()
-      WHERE id = ${id};
-    `;
-  }
-
-  public delete(id: string) {
-    return this.db.query`
-      DELETE FROM users
-      WHERE id = ${id};
-    `;
+    return this.factory.toObject(result);
   }
 }
 
-const userModel = new UserModel(db, UserFactory);
+const userModel = new UserModel(db, userFactory);
 
 export type IUserModel = InstanceType<typeof UserModel>;
 export { userModel };
