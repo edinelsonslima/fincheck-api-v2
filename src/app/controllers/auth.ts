59048ece6800b062393cfeeb9c7d03ddfbd469c2
edinelsonslima <edinelsonlima86@gmail.com>
2024-03-12 21:24:10 -0300

    feat: create signup method

--- src/app/controllers/auth.ts
+++ src/app/controllers/auth.ts
@@ -1,23 +1,45 @@
-import { IUserModel, userModel } from '@models/users';
+import { IAuthService, authService } from '@services/auth';
 import { Request, Response } from 'express';
 
 class AuthController {
-  constructor(private readonly userModel: IUserModel) {}
+  constructor(private readonly authService: IAuthService) {
+    this.signin = this.signin.bind(this);
+    this.signup = this.signup.bind(this);
+  }
 
-  public signin = async (req: Request, res: Response) => {
-    const { email, password } = req.body;
+  public async signin(req: Request, res: Response) {
+    try {
+      const { email, password } = req.body;
 
-    const user = await this.userModel.get({
-      email: 'teste@gmail.com',
-      id: '123',
-      name: 'teste',
-    });
-    res.json(user);
-  };
+      const accessToken = await this.authService.signin({
+        email,
+        password,
+      });
 
-  public async signup(req: Request, res: Response) {}
+      res.status(201).json({ accessToken });
+    } catch (error: any) {
+      const err: Error = error;
+      res.status(401).json({ message: err.message });
+    }
+  }
+
+  public async signup(req: Request, res: Response) {
+    try {
+      const { name, email, password } = req.body;
+
+      const { accessToken } = await this.authService.signup({
+        name,
+        email,
+        password,
+      });
+
+      res.status(201).json({ accessToken });
+    } catch (error: any) {
+      const err: Error = error;
+      res.status(400).json({ message: err.message });
+    }
+  }
 }
 
-const authController = new AuthController(userModel);
-
-export { authController };
+export type IAuthController = InstanceType<typeof AuthController>;
+export const authController = new AuthController(authService);
