ce355ec50eae3f00d1800e47f9d0e4bc028c81cb
edinelsonslima <edinelsonlima86@gmail.com>
2024-03-16 16:04:43 -0300

    refactor: change factory for mapper

--- src/app/repositories/bank-account.ts
+++ src/app/repositories/bank-account.ts
@@ -1,12 +1,9 @@
 import {
-  IBankAccountFactory,
-  bankAccountFactory,
-} from '@factories/bank-account';
-import {
-  IBankAccount,
+  IBankAccountMapperPersistence,
   ICreateBankAccountBody,
   IUpdateBankAccountBody,
 } from '@interfaces/bank-account';
+import { IBankAccountMapper, bankAccountMapper } from '@mappers/bank-account';
 import { IDatabase, db } from 'database';
 
 interface ICreateBankAccount extends ICreateBankAccountBody {
@@ -16,17 +13,17 @@ interface ICreateBankAccount extends ICreateBankAccountBody {
 class BankAccountRepository {
   constructor(
     private readonly db: IDatabase,
-    private readonly factory: IBankAccountFactory
+    private readonly mapper: IBankAccountMapper
   ) {}
 
   public async findAllByUserId(userId: string) {
-    const result = await this.db.query<IBankAccount>`
-      SELECT bank_accounts.*, transactions.type, transactions.name FROM bank_accounts
+    const result = await this.db.query<IBankAccountMapperPersistence>`
+      SELECT * FROM bank_accounts
       LEFT JOIN transactions ON bank_accounts.id = transactions.bank_account_id
       WHERE bank_accounts.user_id = ${userId};
     `;
 
-    return this.factory.toArray(result);
+    return this.mapper.toArray(result);
   }
 
   public async create({
@@ -36,25 +33,25 @@ class BankAccountRepository {
     type,
     userId,
   }: ICreateBankAccount) {
-    const result = await this.db.query<IBankAccount>`
+    const result = await this.db.query<IBankAccountMapperPersistence>`
       INSERT INTO bank_accounts (name, initial_balance, type, color, user_id)
       OUTPUT INSERTED.*
       VALUES (${name}, ${initialBalance}, ${type}, ${color}, ${userId})
     `;
 
-    return this.factory.toObject(result);
+    return this.mapper.toObject(result);
   }
 
   public async findOneByUserIdAndBankAccountId(
     userId: string,
     bankAccountId: string
   ) {
-    const result = await this.db.query<IBankAccount>`
+    const result = await this.db.query<IBankAccountMapperPersistence>`
       SELECT TOP 1 * FROM bank_accounts
       WHERE bank_accounts.user_id = ${userId} AND bank_accounts.id = ${bankAccountId};
     `;
 
-    return this.factory.toObject(result);
+    return this.mapper.toObject(result);
   }
 
   public async updateByUserIdAndBankAccountId(
@@ -62,7 +59,7 @@ class BankAccountRepository {
     bankAccountId: string,
     { color, initialBalance, name, type }: IUpdateBankAccountBody
   ) {
-    const result = await this.db.query<IBankAccount>`
+    const result = await this.db.query<IBankAccountMapperPersistence>`
       UPDATE bank_accounts
       SET name = ISNULL(${name ?? null}, name),
           initial_balance = ISNULL(${initialBalance ?? null}, initial_balance),
@@ -72,14 +69,14 @@ class BankAccountRepository {
       WHERE bank_accounts.user_id = ${userId} AND bank_accounts.id = ${bankAccountId};
     `;
 
-    return this.factory.toObject(result);
+    return this.mapper.toObject(result);
   }
 
   public async deleteByUserIdAndBankAccountId(
     userId: string,
     bankAccountId: string
   ) {
-    const result = await this.db.query<IBankAccount>`
+    const result = await this.db.query<IBankAccountMapperPersistence>`
       IF EXISTS (
         SELECT 1 FROM bank_accounts
         WHERE bank_accounts.user_id = ${userId} AND bank_accounts.id = ${bankAccountId}
@@ -95,5 +92,5 @@ class BankAccountRepository {
 export type IBankAccountRepository = InstanceType<typeof BankAccountRepository>;
 export const bankAccountRepository = new BankAccountRepository(
   db,
-  bankAccountFactory
+  bankAccountMapper
 );
