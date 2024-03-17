0f6b1406689f5dc054dc969aae55891456a5b6b9
edinelsonslima <edinelsonlima86@gmail.com>
2024-03-16 21:12:08 -0300

    feat: create validator for queries string

--- src/middleware/validators.ts
+++ src/middleware/validators.ts
@@ -1,7 +1,7 @@
 import { env } from 'app/settings';
 import { NextFunction, Request, Response } from 'express';
 import { verify } from 'jsonwebtoken';
-import { ZodSchema, z } from 'zod';
+import { ZodSchema, unknown, z } from 'zod';
 
 interface IAuthorizationPayload {
   userId: string;
@@ -17,15 +17,7 @@ class validate {
 
         next();
       } catch (err) {
-        if (!(err instanceof z.ZodError)) {
-          return res.status(500).json({ error: 'Internal Server Error' });
-        }
-
-        res.status(400).json({
-          message: err.errors.reduce((acc, err) => {
-            return { ...acc, [err.path.toString()]: err.message };
-          }, {}),
-        });
+        return this.handleZodError(err, res);
       }
     };
   }
@@ -38,15 +30,20 @@ class validate {
 
         next();
       } catch (err) {
-        if (!(err instanceof z.ZodError)) {
-          return res.status(500).json({ error: 'Internal Server Error' });
-        }
+        return this.handleZodError(err, res);
+      }
+    };
+  }
 
-        res.status(400).json({
-          message: err.errors.reduce((acc, err) => {
-            return { ...acc, [err.path.toString()]: err.message };
-          }, {}),
-        });
+  public static query<T>(validator: ZodSchema<T>) {
+    return async (req: Request, res: Response, next: NextFunction) => {
+      try {
+        const data = await validator.parseAsync(req.query);
+        (req.query as T) = data;
+
+        next();
+      } catch (err) {
+        return this.handleZodError(err, res);
       }
     };
   }
@@ -78,6 +75,18 @@ class validate {
   private static getAuthorizationToken = (req: Request) => {
     const [type, token] = req.headers.authorization?.split(' ') ?? [];
     return type === 'Bearer' ? token : undefined;
+  };
+
+  private static handleZodError = (err: any, res: Response) => {
+    if (!(err instanceof z.ZodError)) {
+      return res.status(500).json({ error: 'Internal Server Error' });
+    }
+
+    const message = err.errors.reduce((acc, err) => {
+      return { ...acc, [err.path.toString()]: err.message };
+    }, {});
+
+    return res.status(400).json(message);
   };
 }
 
