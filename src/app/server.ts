177481388434ee7d58fa12262357729eaca25440
edinelsonslima <edinelsonlima86@gmail.com>
2024-03-17 19:32:16 -0300

    feat: add global handle errors

--- src/app/server.ts
+++ src/app/server.ts
@@ -1,6 +1,11 @@
 /* eslint-disable no-console */
+import 'express-async-errors';
+
+import { enStatusCodes } from '@enums/status-code';
+import { IRequest, IResponse } from '@interfaces/express';
 import cors from 'cors';
-import express from 'express';
+import { DomainError } from 'errors/domain';
+import express, { NextFunction } from 'express';
 import { router } from 'routers';
 import { env } from './settings';
 
@@ -11,6 +16,20 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(router);
+
+app.use((error: any, req: IRequest, res: IResponse, next: NextFunction) => {
+  if (!(error instanceof DomainError)) {
+    console.error(error);
+    return res
+      .status(enStatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ message: 'internal server error' });
+  }
+
+  const { code, message, data } = error.toJSON();
+  return res
+    .status(code)
+    .json({ message, ...(Object.keys(data).length && data) });
+});
 
 app.listen(env.PORT, () => {
   console.log('🚀 server is running on port %s', env.PORT);
